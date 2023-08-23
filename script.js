1f48b8943bc6c924c0c23a10d9843d505305fbf1
Dong Woo Lee <don04lee@gmail.com>
2023-08-22 22:20:46 -0700

    Current prototype of game with board and controls
    Currently made it so player works as a boolean with basically an
    on/off switch to make the marker be X and O, and as of right now,
    it can accurately get the right winner. However, the code is very messy
    and as it is right now, it is very difficult to find a way to add new
    functions.

--- script.js
+++ script.js
@@ -1,36 +1,103 @@
 const gameBoard = (() => {
-  let container = document.getElementById('container');
-  const board = document.createElement('div');
-  board.classList.add('board');
-  
-  const createBoard = () => {
-    for(let i = 0; i < 3; i++) {
-      let row = document.createElement('div');
-      row.classList.add('row');
-      for(let j = 0; j < 3; j++) {
-        let cell = document.createElement('div');
-        cell.textContent = '';
-        cell.style.height = `200px`;
-        cell.style.width = `200px`;
-        cell.classList.add('cell');
-        row.appendChild(cell);
+
+  const board = ["", "", "", "", "", "", "", "", ""];
+  const endLogic = [[0, 1, 2], 
+                    [3, 4, 5], 
+                    [6, 7, 8],
+                    [0, 3, 6],
+                    [1, 4, 7],
+                    [2, 5, 8],
+                    [0, 4, 8],
+                    [2, 4, 6]]
+
+  const getBoard = () => {
+    return board;
+  }
+
+  const getBoardIdx = (idx) => {
+    return board[idx];
+  }
+
+  const setBoard = (cell, player) => {
+    board[cell.id] = player;
+  }
+
+  function endBoard(cell) {
+    let curr = document.getElementById(cell);
+    curr.textContent = "LOL";
+  }
+
+  function checkStatus() {
+    for(let i = 0; i < endLogic.length; i++) {
+      if(getBoardIdx(endLogic[i][0]).length !== 0 && getBoardIdx(endLogic[i][1]).length !== 0 && getBoardIdx(endLogic[i][2]).length !== 0) {
+        if(getBoardIdx(endLogic[i][0]) === getBoardIdx(endLogic[i][1]) & getBoardIdx(endLogic[i][1]) === getBoardIdx(endLogic[i][2])) {  
+          return true;
+        }
       }
-      board.appendChild(row);
     }
-    container.appendChild(board);
+    return false;
+  }
+
+  function checkEnd() {
+    for(let i = 0; i < board.length; i++) {
+      if(board[i].length == 0) {
+        return false;
+      }
+    }
+    return true;
   }
 
   return {
-    createBoard,
+    getBoard,
+    getBoardIdx,
+    setBoard,
+    endBoard,
+    checkStatus,
+    checkEnd,
   }
 })();
 
-gameBoard.createBoard();
+const gameController = (() => {
 
-const displayController = (() => {
-  
+  let player = true;
+  const cells = document.querySelectorAll('.cell');
+
+  cells.forEach(cell => {
+    cell.addEventListener('click', function() {
+      if(player && gameBoard.getBoardIdx(cell.id) == '') {
+        cell.textContent = '0';
+        player = !player;
+        gameBoard.setBoard(cell, '0');
+      }
+      else if((!player) && gameBoard.getBoardIdx(cell.id).length == 0) {
+        cell.textContent = 'X';
+        player = !player;
+        gameBoard.setBoard(cell, 'X');
+      }
+      if(gameBoard.checkStatus()) {
+        if(!player) {
+          console.log("Player \'0\' wins!");
+        }
+        else {
+          console.log("Player \'X\' wins!");
+        }
+      }
+      else if(gameBoard.checkEnd()) {
+        endGame();
+      }
+    });
+  })
+
+  function endGame() {
+    for(let i = 0; i < gameBoard.getBoard().length; i++) {
+      gameBoard.endBoard(i);
+    }
+  }
+
+  return {
+    endGame,
+  }
+
 })();
 
-const playerFactory = () => {
-
-};
+const game = gameController;
